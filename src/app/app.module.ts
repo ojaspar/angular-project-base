1ccd2772882bf2e22f83666bc5412433180f1656
Divine Ogbonna <dogbonna@sbsc.com>
2020-01-29 10:54:55 +0100

    registered auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpErrorInterceptor } from "./core/interceptor/http-error.interceptor";
+import { TokenInterceptor } from "./core/interceptor/token.interceptor";
+import { MainModule } from "./component/main/main.module";
+import { AuthModule } from "./component/auth/auth.module";
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, MainModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
